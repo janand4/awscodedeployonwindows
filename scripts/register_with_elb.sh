88a562fe7a64bc67002a99287f318a3c65465e7d
Janisha Anand <anand.janisha@gmail.com>
2016-08-01 14:23:18 -0400

    Add files via upload

--- scripts/register_with_elb.sh
+++ scripts/register_with_elb.sh
@@ -31,19 +31,19 @@ msg "Checking if instance $INSTANCE_ID is part of an AutoScaling group"
 asg=$(autoscaling_group_name $INSTANCE_ID)
 if [ $? == 0 -a -n "$asg" ]; then
     msg "Found AutoScaling group for instance $INSTANCE_ID: $asg"
-    
+
     msg "Checking that installed CLI version is at least at version required for AutoScaling Standby"
     check_cli_version
     if [ $? != 0 ]; then
         error_exit "CLI must be at least version ${MIN_CLI_X}.${MIN_CLI_Y}.${MIN_CLI_Z} to work with AutoScaling Standby"
     fi
 
-    msg "Attempting to put instance into Standby"
-    autoscaling_enter_standby $INSTANCE_ID $asg
+    msg "Attempting to move instance out of Standby"
+    autoscaling_exit_standby $INSTANCE_ID $asg
     if [ $? != 0 ]; then
-        error_exit "Failed to move instance into standby"
+        error_exit "Failed to move instance out of standby"
     else
-        msg "Instance is in standby"
+        msg "Instance is no longer in Standby"
         exit 0
     fi
 fi
@@ -52,10 +52,10 @@ msg "Instance is not part of an ASG, continuing..."
 
 msg "Checking that user set at least one load balancer"
 if test -z "$ELB_LIST"; then
-    error_exit "Must have at least one load balancer to deregister from"
+    error_exit "Must have at least one load balancer to register to"
 fi
 
-# Loop through all LBs the user set, and attempt to deregister this instance from them.
+# Loop through all LBs the user set, and attempt to register this instance to them.
 for elb in $ELB_LIST; do
     msg "Checking validity of load balancer named '$elb'"
     validate_elb $INSTANCE_ID $elb
@@ -64,20 +64,20 @@ for elb in $ELB_LIST; do
         continue
     fi
 
-    msg "Deregistering $INSTANCE_ID from $elb"
-    deregister_instance $INSTANCE_ID $elb
+    msg "Registering $INSTANCE_ID to $elb"
+    register_instance $INSTANCE_ID $elb
 
     if [ $? != 0 ]; then
-        error_exit "Failed to deregister instance $INSTANCE_ID from ELB $elb"
+        error_exit "Failed to register instance $INSTANCE_ID from ELB $elb"
     fi
 done
 
-# Wait for all Deregistrations to finish
-msg "Waiting for instance to de-register from its load balancers"
+# Wait for all Registrations to finish
+msg "Waiting for instance to register to its load balancers"
 for elb in $ELB_LIST; do
-    wait_for_state "elb" $INSTANCE_ID "OutOfService" $elb
+    wait_for_state "elb" $INSTANCE_ID "InService" $elb
     if [ $? != 0 ]; then
-        error_exit "Failed waiting for $INSTANCE_ID to leave $elb"
+        error_exit "Failed waiting for $INSTANCE_ID to return to $elb"
     fi
 done
 
